91a89806b91c16fb38a680d72c7da182d990ddc9
digital-one <neil@digital-one.co.uk>
2014-11-19 21:57:28 +0000

    HTML build

---
+++ templates/what-we-do.php
@@ -0,0 +1,71 @@
+<!doctype html>
+<!--[if lt IE 7 ]> <html lang="en" class="no-js ie6"> <![endif]-->
+<!--[if IE 7 ]>    <html lang="en" class="no-js ie7"> <![endif]-->
+<!--[if IE 8 ]>    <html lang="en" class="no-js ie8"> <![endif]-->
+<!--[if IE 9 ]>    <html lang="en" class="no-js ie9"> <![endif]-->
+<!--[if (gt IE 9)|!(IE)]><!--> <html lang="en" class="no-js"> <!--<![endif]-->
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        
+        <title></title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+        
+        <!-- Place favicon.ico and apple-touch-icon(s) in the root directory -->
+        <link rel="stylesheet" href="css/layout.css" />
+        <link rel="stylesheet" href="js/slick/slick.css" />
+        <script src="js/modernizr.js"></script>
+<!--[if (gte IE 6)&(lte IE 8)]>
+<script src="js/selectivizr-min.js"></script>
+<![endif]-->
+        <!--[if lte IE 9]>
+          <script src="js/respond.min.js"></script>
+        <![endif]-->
+    </head>
+    <body>
+    <div id="page-wrap">
+        <header id="header">
+        <h1 id="home-link"><img src="images/shark.svg" alt="Shark Design" /></h1>
+        <!--<a id="home-link" href="">Shark Design</a>-->
+    </header>
+           <aside id="sidebar">
+<nav id="nav">
+    <ul>
+<li class="current-menu-item"><a href="">Home</a></li>
+<li><a href="">Who we are</a></li>
+<li><a href="">What we do</a></li>
+<li><a href="">Our work</a></li>
+<li><a href="">Get in touch</a></li>
+<li><a href="">Enlightenment</a></li>
+</ul>
+</nav>
+
+
+<section id="contacts">
+<nav id="social">
+<ul><li><a href="" class="facebook">Facebook</a></li>
+    <li><a href="" class="twitter">Twitter</a></li>   
+    <li><a href="" class="linkedin">Linkedin</a></li>
+    <li><a href="" class="email">Email</a></li>
+</ul>
+</nav>
+<small><a href="tel:01422376446">01422 376446</a>
+<a href="mailto:enquiries@sharkdesign.co.uk">enquiries@sharkdesign.co.uk</a></small>
+</section>
+        </aside>
+<div class="section" data-anchor="" data-title="">
+<section class="page-title blue">
+<div><h1>What we do</h1><h2 class="underline">out thinking your competition</h2></div>
+</section>
+ <section class="banner" style="background-image:url('images/what-we-do.jpg');">image</section>   
+<section class="content">
+    <dl id="sectors">
+<dt><div><h3>Brand Strategy</h3><h4 class="underline">Lorem ipsum dolor sit amet</h4></div></dt>
+<dd><div class="main"><p>Lorem ipsum, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud co laboris nisi ut aliquip ex ea commodo consequat. e dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p><a href="" class="button">Read Case Study</a></div><aside><ul><li>Market Analysis</li><li>Predatory Thinking</li></ul></aside></dd>
+</dl>
+    </section>
+</div>
+</div>
+    </body>
+    </html>
---
+++ templates/who-we-are.php
@@ -0,0 +1,66 @@
+<!doctype html>
+<!--[if lt IE 7 ]> <html lang="en" class="no-js ie6"> <![endif]-->
+<!--[if IE 7 ]>    <html lang="en" class="no-js ie7"> <![endif]-->
+<!--[if IE 8 ]>    <html lang="en" class="no-js ie8"> <![endif]-->
+<!--[if IE 9 ]>    <html lang="en" class="no-js ie9"> <![endif]-->
+<!--[if (gt IE 9)|!(IE)]><!--> <html lang="en" class="no-js"> <!--<![endif]-->
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        
+        <title></title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+        
+        <!-- Place favicon.ico and apple-touch-icon(s) in the root directory -->
+        <link rel="stylesheet" href="css/layout.css" />
+        <link rel="stylesheet" href="js/slick/slick.css" />
+        <script src="js/modernizr.js"></script>
+<!--[if (gte IE 6)&(lte IE 8)]>
+<script src="js/selectivizr-min.js"></script>
+<![endif]-->
+        <!--[if lte IE 9]>
+          <script src="js/respond.min.js"></script>
+        <![endif]-->
+    </head>
+    <body>
+    <div id="page-wrap">
+        <header id="header">
+        <h1 id="home-link"><img src="images/shark.svg" alt="Shark Design" /></h1>
+        <!--<a id="home-link" href="">Shark Design</a>-->
+    </header>
+           <aside id="sidebar">
+<nav id="nav">
+    <ul>
+<li class="current-menu-item"><a href="">Home</a></li>
+<li><a href="">Who we are</a></li>
+<li><a href="">What we do</a></li>
+<li><a href="">Our work</a></li>
+<li><a href="">Get in touch</a></li>
+<li><a href="">Enlightenment</a></li>
+</ul>
+</nav>
+
+
+<section id="contacts">
+<nav id="social">
+<ul><li><a href="" class="facebook">Facebook</a></li>
+    <li><a href="" class="twitter">Twitter</a></li>   
+    <li><a href="" class="linkedin">Linkedin</a></li>
+    <li><a href="" class="email">Email</a></li>
+</ul>
+</nav>
+<small><a href="tel:01422376446">01422 376446</a>
+<a href="mailto:enquiries@sharkdesign.co.uk">enquiries@sharkdesign.co.uk</a></small>
+</section>
+        </aside>
+<div class="section" data-anchor="" data-title="">
+<section class="page-title blue">
+<div><h1>Who we are</h1><h2 class="underline">a little bit about us</h2></div>
+</section>
+ <section class="banner">image</section>   
+<section class="content"><div class="content-wrap"><div class="column"><h3>A Friendly Bunch</h3><p>Lorem ipsum, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad </p></div><div class="column"><h3>Full of passion</h3><p>Lorem ipsum, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad </p></div><div class="column"><h3>We love a challenge</h3><p>Lorem ipsum, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad </p></div></div></section>
+</div>
+</div>
+    </body>
+    </html>
